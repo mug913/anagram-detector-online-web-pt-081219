e8115c1c0b7736ffba5dbd8c6e4bbd0420c7dd56
Brad Alexander <brad.alexander913@gmail.com>
2019-09-05 02:14:35 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,10 +1,25 @@
 # Your code goes here!
+require 'pry'
+
+
 class Anagram 
   
-  attr_accessor
+  attr_accessor :word
   
+  def initialize(word)
+    @word = word 
+  end
   
-  
-  
+  def match(options)
+    matches = []
+    options.each_with_index do |option, index|
+      letter_array = option.split("")
+      word_array = @word.split("")
+      if letter_array.sort == word_array.sort 
+        matches << option
+      end 
+    end 
+    matches
+  end
   
 end
