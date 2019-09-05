f0587f4af210fe2d6420669cb678860edc235598
Brad Alexander <brad.alexander913@gmail.com>
2019-09-05 02:08:30 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,4 +1,7 @@
 # Your code goes here!
+require 'pry'
+
+
 class Anagram 
   
   attr_accessor :word
@@ -12,6 +15,7 @@ class Anagram
       letter_array = option.split
       word_array = @word.split
       if letter_array.sort != word_array.sort 
+        binding.pry
         options.delete_at(index) 
       end 
     end 
